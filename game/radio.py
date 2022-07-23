6313bac61911e33075c9a52a3242095766727c14
Chateauvisionn <chateauvision@outlook.fr>
2022-07-23 20:33:44 +0200

    added radio song

--- game/radio.py
+++ game/radio.py
@@ -1,6 +1,6 @@
 from ursina import *
 
-class Radio(Button):
+class Radio(Entity):
     def __init__(self, position=(0,0,0)):
         super().__init__(
             model='Radio',
@@ -10,3 +10,9 @@ class Radio(Button):
             color=color.white,
         )
         self.collider=BoxCollider(self, size=(20,16,10), center=(0,-8,15))
+        self.song = Audio("looping_radio_mix.wav", loop=True)
+
+        self.song.play()
+    
+    def on_disable(self):
+        self.song.stop()
